dd619f9f74145bbb3431642162165c1052e40b7c
Abdelilah <76753930+abdelilahdahdahi@users.noreply.github.com>
2023-01-02 13:40:31 +0100

    Update index.js

--- index.js
+++ index.js
@@ -8,44 +8,44 @@ let randomNumber2 = Math.round(Math.random() * 6);
 // launching the dice for the player 1
 switch (randomNumber1) {
     case 1:
-        document.querySelector("#dadoPlayer1").setAttribute("src", "https://i.ibb.co/68818gK/dice1.png");
+        document.querySelector("#dadoPlayer1").setAttribute("src", "images/dice1.png");
         break;
     case 2:
-        document.querySelector("#dadoPlayer1").setAttribute("src", "https://i.ibb.co/7tjg85n/dice2.png");
+        document.querySelector("#dadoPlayer1").setAttribute("src", "images/dice2.png");
         break;
     case 3:
-        document.querySelector("#dadoPlayer1").setAttribute("src", "https://i.ibb.co/kyPrKzY/dice3.png");
+        document.querySelector("#dadoPlayer1").setAttribute("src", "images/dice3.png");
         break;
     case 4:
-        document.querySelector("#dadoPlayer1").setAttribute("src", "https://i.ibb.co/BNzY8Nv/dice4.png");
+        document.querySelector("#dadoPlayer1").setAttribute("src", "images/dice4.png");
         break;
     case 5:
-        document.querySelector("#dadoPlayer1").setAttribute("src", "https://i.ibb.co/bmCfCbn/dice5.png");
+        document.querySelector("#dadoPlayer1").setAttribute("src", "images/dice5.png");
         break;
     case 6:
-        document.querySelector("#dadoPlayer1").setAttribute("src", "https://i.ibb.co/zQyzMD3/dice6.png");
+        document.querySelector("#dadoPlayer1").setAttribute("src", "images/dice6.png");
         break;
 }
 
 // launching the dice for the player 2
 switch (randomNumber2) {
     case 1:
-        document.querySelector("#dadoPlayer2").setAttribute("src", "https://i.ibb.co/68818gK/dice1.png");
+        document.querySelector("#dadoPlayer2").setAttribute("src", "images/dice1.png");
         break;
     case 2:
-        document.querySelector("#dadoPlayer2").setAttribute("src", "https://i.ibb.co/7tjg85n/dice2.png");
+        document.querySelector("#dadoPlayer2").setAttribute("src", "images/dice2.png");
         break;
     case 3:
-        document.querySelector("#dadoPlayer2").setAttribute("src", "https://i.ibb.co/kyPrKzY/dice3.png");
+        document.querySelector("#dadoPlayer2").setAttribute("src", "images/dice3.png");
         break;
     case 4:
-        document.querySelector("#dadoPlayer2").setAttribute("src", "https://i.ibb.co/BNzY8Nv/dice4.png");
+        document.querySelector("#dadoPlayer2").setAttribute("src", "images/dice4.png");
         break;
     case 5:
-        document.querySelector("#dadoPlayer2").setAttribute("src", "https://i.ibb.co/bmCfCbn/dice5.png");
+        document.querySelector("#dadoPlayer2").setAttribute("src", "images/dice5.png");
         break;
     case 6:
-        document.querySelector("#dadoPlayer2").setAttribute("src", "https://i.ibb.co/zQyzMD3/dice6.png");
+        document.querySelector("#dadoPlayer2").setAttribute("src", "images/dice6.png");
         break;
 }
 
