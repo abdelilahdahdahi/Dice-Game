f239122d420ba2985d58f4c3fd08eb9e4a1c0888
Abdelilah <76753930+abdelilahdahdahi@users.noreply.github.com>
2023-01-07 13:28:45 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // launching the dice for the 1st player
-let randomNumber1 = Math.round(Math.random() * 6); // generating a random number 0-6
+let randomNumber1 = Math.round(Math.random() * 5) + 1; // generating a random number 1-6
 let randomDice1 = "/images/dice" + randomNumber1 + ".png";
 document.querySelector("#dadoPlayer1").setAttribute("src", randomDice1);
 
 // launching the dice for the 2nd player
-let randomNumber2 = Math.round(Math.random() * 6);
+let randomNumber2 = Math.round(Math.random() * 5) +1;
 let randomDice2 = "/images/dice" + randomNumber2 + ".png";
 document.querySelector("#dadoPlayer2").setAttribute("src", randomDice2);
 
